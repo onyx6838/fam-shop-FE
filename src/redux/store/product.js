e227103ffc40a7a90e891f2093180292ea88d80c
Giang Doan Minh <giangdm@officevnpay.com>
2022-03-09 11:22:08 +0700

    redux product

--- src/redux/store/product.js
+++ src/redux/store/product.js
@@ -1,14 +1,16 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
-import CartApi from '../../api/CartApi'
+import SanPhamApi from '../../api/SanPhamApi'
 
-export const getProducts = createAsyncThunk('product/get', async (data, { rejectWithValue }) => {
-
-    const response = await CartApi.getAll();
+/**
+ * thunk
+ */
+export const fetchProductsFilter = createAsyncThunk('product/fetchFilter', async (data, { rejectWithValue }) => {
+    const { page, size } = data;
+    const response = await SanPhamApi.getAll({ page, size });
     if (response.status < 200 || response.status >= 300) {
         return rejectWithValue(response);
     }
-
     return response;
 })
 
@@ -16,8 +18,10 @@ const slice = createSlice({
     name: 'product',
     initialState: {
         products: [],
+        totalElements: 0,
+        totalPages: 0,
         page: 1,
-        size: 3,
+        size: 6,
         // filter by category
         category: 0,
         // filter by feature
@@ -26,8 +30,11 @@ const slice = createSlice({
     reducers: {
     },
     extraReducers: {
-        [getProducts.fulfilled]: (state, action) => {
-            state.products = action.payload;
+        [fetchProductsFilter.fulfilled]: (state, action) => {
+            state.products = action.payload.content;
+            state.totalElements = action.payload.totalElements;
+            state.totalPages = action.payload.totalPages;
+            state.page = action.payload.page;
         }
     }
 });
